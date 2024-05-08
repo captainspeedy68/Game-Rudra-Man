fefe8914c41cf7df839924a5deec5ed64ad92be1
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-08 19:04:30 +0600

    start adding the fighter class and 2 instances

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from fighter import Fighter
 pygame.init()
 
 # create game window
@@ -12,13 +13,24 @@ pygame.display.set_caption("Rudra Man")
 bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
 
 #function for drawing background
-
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (screen_width, screen_height))
+    screen.blit(scaled_bg, (0, 0))
+    
+#create 2 instances of fighter
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(200, 310)
 
 #game loop
 run = True
 
 while run:
+    #draw backgroun
+    draw_bg()
+    
+    #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
 pygame.quit()
