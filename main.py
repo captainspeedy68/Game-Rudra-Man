5fc3d3c574e78794a2a8ee9635902cd670f18137
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-05-08 21:00:06 +0600

    fighters images added and scaled

--- main.py
+++ main.py
@@ -9,60 +9,79 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Rudra Man")
 
-#set framerate
+# set framerate
 clock = pygame.time.Clock()
 FPS = 60
 
-#define colors
+# define colors
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 
-#load bg image
-bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+#define fighter variables
+WARRIOR_SIZE = 162
+WARRIOR_SCALE= 4
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE]
 
-#load spreadshits
-warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
-wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").convert_alpha()
+# load bg image
+bg_image = pygame.image.load(
+    "assets/images/background/background.jpg").convert_alpha()
 
-#function for drawing background
+# load spreadshits
+warrior_sheet = pygame.image.load(
+    "assets/images/warrior/Sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load(
+    "assets/images/wizard/Sprites/wizard.png").convert_alpha()
+
+# define number of steps in each animation
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+
+# function for drawing background
+
+
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (screen_width, screen_height))
     screen.blit(scaled_bg, (0, 0))
-    
-#function for drawing health bars
+
+# function for drawing health bars
+
+
 def draw_health_bar(health, x, y):
-    ratio  = health / 100
+    ratio = health / 100
     pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
     pygame.draw.rect(screen, RED, (x, y, 400, 30))
     pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
-    
-    
-#create 2 instances of fighter
-fighter_1 = Fighter(200, 310)
-fighter_2 = Fighter(700, 310)
 
-#game loop
+
+# create 2 instances of fighter
+fighter_1 = Fighter(200, 310, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
+fighter_2 = Fighter(700, 310, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
+
+# game loop
 run = True
 
 while run:
     clock.tick(FPS)
-    #draw backgroun
+    # draw backgroun
     draw_bg()
-    
-    #show player stats
+
+    # show player stats
     draw_health_bar(fighter_2.health, 20, 20)
     draw_health_bar(fighter_1.health, 580, 20)
-    
-    #move fighters
+
+    # move fighters
     fighter_1.move(screen_width, screen_height, screen, fighter_2)
     # fighter_2.move()
-    
-    #draw fighters
+
+    # draw fighters
     fighter_1.draw(screen)
     fighter_2.draw(screen)
-    
-    #event handler
+
+    # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+d(
+#     "assets/images/warrior/Sprites/warrior.png").convert_alpha()
+# wizard_sheet = pygame.image.load(
+#     "assets/images/wizard/Sprites/wizard.png").convert_alpha()
